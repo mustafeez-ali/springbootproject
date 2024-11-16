0304445613c04d309c4b65325de4f914f2800bd5
mustafeez-ali <mustafeezalig88@gmail.com>
2024-11-16 21:52:05 +0530

    MYSQL security changes

--- spring-boot-course-application/src/main/java/com/techm/controller/CourseController.java
+++ spring-boot-course-application/src/main/java/com/techm/controller/CourseController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,6 +30,7 @@ public class CourseController {
 	@Autowired
 	RestTemplate restTemplate;
 
+	//@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/courses")
 	private List<Course> getAllBooks() {
 		System.out.println("TEST");
---
+++ spring-boot-course-application/src/main/java/com/techm/security/config/User.java
@@ -0,0 +1,67 @@
+package com.techm.security.config;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+ 
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ 
+    public User() {
+		super();
+		
+	}
+	private String username;
+    private String password;
+    private String role;
+    private boolean enabled;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public User(Long id, String username, String password, String role, boolean enabled) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+		this.enabled = enabled;
+	}
+ 
+   
+ 
+}
