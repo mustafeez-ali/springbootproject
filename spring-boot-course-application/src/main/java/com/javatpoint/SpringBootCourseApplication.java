76e3b6fc5d771009f1df5941b7f17bdf2dcf9967
mustafeez-ali <mustafeezalig88@gmail.com>
2025-07-08 07:33:30 +0530

    demo

--- spring-boot-course-application/src/main/java/com/javatpoint/SpringBootCourseApplication.java
+++ spring-boot-course-application/src/main/java/com/javatpoint/SpringBootCourseApplication.java
@@ -1,6 +1,7 @@
 package com.javatpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SpringBootCourseApplication 
 {
